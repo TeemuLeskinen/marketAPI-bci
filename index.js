ca48dd38f62eecc2a7543df3e82779e608676d2d
Teemu Leskinen <t8lete03@students.oamk.fi>
2021-02-26 13:00:07 +0200

    Url updated

--- index.js
+++ index.js
@@ -2,22 +2,3 @@ const server = require('./server');
 server.start();
 
 
-/*const express = require('express');
-const app = express();
-const port = 3000;
-
-const usersRouter = require('./routes/users.js');
-const postingsRouter = require('./routes/postings.js');
-const fs = require('fs');
-const bodyParser = require('body-parser');
-
-app.use(bodyParser.json());
-
-app.use(usersRouter);
-app.use(postingsRouter);
-
-
-
-app.listen(port, () => {
-    console.log(`API listening on port ${port}`);
-});*/
