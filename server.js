65b5734b5b2b327cbaee3110ca9eca974b36a200
Teemu Leskinen <t8lete03@students.oamk.fi>
2021-02-26 10:29:57 +0200

    Set up for heroku

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+//const port = 3000;
 
 const router = require('./routes/users.js');
 const fs = require('fs');
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.json());
+app.set('port', (process.env.PORT || 80));
 
+app.use(bodyParser.json());
 app.use(router);
 
 let serverInstance = null
 
 module.exports = {
     start: () => {
-        serverInstance = app.listen(port, () => {
-            console.log(`API listening on port ${port}`)
+        serverInstance = app.listen(app.get('port'), () => {
+            console.log('API running on port', app.get('port'));
     })
     },
     close: () => {
